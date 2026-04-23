4e8954c23477600a217edf7d9d70eb758eca84cb
samrad29 <sam.radtke99@gmail.com>
2026-04-23 16:20:23 -0500

    fixing opportunity id for the oei pages

--- pipelines/wi_psc/db_util.py
+++ pipelines/wi_psc/db_util.py
@@ -185,7 +185,7 @@ def save_ai_extraction(conn, extraction: dict, url: str, webpage_text_hash: str)
         """,
         (
             "wi_psc_oei",
-            f"WI_OEI_-{program_name}",
+            f"WI-PSC-OEI-{program_name}",
             -1,
             program_name,
             program_status,

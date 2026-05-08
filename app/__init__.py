99a3b57c16e038982fbc90cd51d74943b4437e1d
samrad29 <sam.radtke99@gmail.com>
2026-05-07 22:07:53 -0500

    adding db_migration blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from app.routes.dashboard import dashboard_bp
 from app.routes.api import api_bp
 from app.routes.user_activity import user_activity_bp
+from app.routes.db_migration import db_migration_bp
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +13,5 @@ def create_app():
     app.register_blueprint(dashboard_bp)
     app.register_blueprint(api_bp)
     app.register_blueprint(user_activity_bp)
+    app.register_blueprint(db_migration_bp)
     return app
